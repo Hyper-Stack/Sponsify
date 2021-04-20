e4800158f1a883eb1991b67bd74a6f17e953af47
Anubhav2907 <76243969+Anubhav2907@users.noreply.github.com>
2021-04-20 13:23:49 +0530

    manager page

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -39,3 +39,27 @@ $("#search-icon").click(function() {
        }
    });
 });
+placeCards = () => {
+    let wrappers = document.querySelectorAll(".wrapper");
+    [...wrappers].forEach(wrapper => {
+      wrapper.classList.add("frontback-cards");
+    });
+  };
+  
+  // toggle class for flip animation
+  flipCard = () => {
+    let cardContainers = document.querySelectorAll(".card-container");
+    [...cardContainers].forEach(card => {
+      let cardBtns = card.querySelectorAll('.btn-details');
+      cardBtns.forEach(btn => {
+        btn.addEventListener('click', function () {
+          card.classList.toggle("addFlip");
+        }, false);
+      });
+    });
+  };
+  
+  (() => {
+    placeCards();
+    flipCard();
+  })();
